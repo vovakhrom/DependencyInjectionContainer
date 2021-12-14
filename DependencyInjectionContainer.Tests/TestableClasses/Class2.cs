9022a879f9b01af07aca5f02b376f3b080eb3d1f
Vladimir <lililolo1222@mail.ru>
2021-12-14 23:48:25 +0300

    Add Test Class2.cs

---
+++ DependencyInjectionContainer.Tests/TestableClasses/Class2.cs
@@ -0,0 +1,9 @@
+﻿using DependencyInjectionContainer.Attribute;
+
+namespace DependencyInjectionContainer.Tests.TestableClasses
+{
+    public class Class2
+    {
+        public Class2(Class1 class1) {}
+    }
+}
