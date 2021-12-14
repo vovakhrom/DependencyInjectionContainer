40c4d0a476be863ecbbc9a030b5671319aff81f1
Vladimir <lililolo1222@mail.ru>
2021-12-14 23:49:44 +0300

    Add MyRepository.cs

---
+++ DependencyInjectionContainer.Tests/TestableClasses/MyRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DependencyInjectionContainer.Tests.TestableClasses
+{
+    public class MyRepository : IRepository
+    {
+        public MyRepository() {}
+        public override string ToString()
+        {
+            return "MY_ReeeeeeeeeP";
+        }
+    }
+}
