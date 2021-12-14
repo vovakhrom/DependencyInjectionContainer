3eb904b1efdf7e6383c492ea8aa18ca866e5499d
Vladimir <lililolo1222@mail.ru>
2021-12-14 23:47:33 +0300

    Add DependencyException.cs

---
+++ DependencyInjectionContainer/Exception/DependencyException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DependencyInjectionContainer.Exception
+{
+    [Serializable]
+    public class DependencyException : System.Exception
+    {
+        public DependencyException()
+        {
+        }
+
+        protected DependencyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public DependencyException(string message) : base(message)
+        {
+        }
+
+        public DependencyException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
