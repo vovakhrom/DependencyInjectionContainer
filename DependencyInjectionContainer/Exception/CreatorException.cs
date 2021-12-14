bf69d824f26028fb40f7bb6e43d5d3cd0e3ed2c0
Vladimir <lililolo1222@mail.ru>
2021-12-14 23:47:12 +0300

    Add CreatorException.cs

---
+++ DependencyInjectionContainer/Exception/CreatorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DependencyInjectionContainer.Exception
+{
+    [Serializable]
+    public class CreatorException : System.Exception
+    {
+        public CreatorException()
+        {
+        }
+
+        protected CreatorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public CreatorException(string message) : base(message)
+        {
+        }
+
+        public CreatorException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
