3e01df8d4ae23d47375ec17491147fccf2be2b1a
Vladimir <lililolo1222@mail.ru>
2021-12-14 23:46:28 +0300

    Add Dependency.cs

---
+++ DependencyInjectionContainer/Dependency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DependencyInjectionContainer
+{
+    public class Dependency
+    {
+        public Type Type { get; }
+
+        public LifeType LifeType { get; }
+
+        public object Key { get; }
+
+        //это поле используется в случае singlton , оно там будет храниться
+        public object Instance { get; set; }
+
+        public Dependency(Type type, LifeType lifeType, object key)
+        {
+            Key = key;
+            Type = type;
+            LifeType = lifeType;
+        }
+    }
+}
