373f40f9d99715dfb5b15441d156c2a2f2337785
Vladimir <lililolo1222@mail.ru>
2021-12-14 23:51:11 +0300

    Add other files from container

---
+++ DependencyInjectionContainer/Attribute/DependencyKeyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DependencyInjectionContainer.Attribute
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class DependencyKeyAttribute : System.Attribute
+    {
+        public object Key { get; }
+
+        public DependencyKeyAttribute(object key)
+        {
+            Key = key;
+        }
+    }
+
+}
